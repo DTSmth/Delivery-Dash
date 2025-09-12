01e0d1dfbd459cd1add738d40ae8398dc8fb1392
DTSmth <ducktapedave85@gmail.com>
2025-09-12 18:59:36 -0400

    Add package pickup dropoff logic

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Package"))
         {
             Debug.Log("Picked up package");
+            hasPackage = true;
         }
-        if (collision.CompareTag("Customer"))
+        if (collision.CompareTag("Customer") && hasPackage)
         {
+            hasPackage = false;
             Debug.Log("Dropped off wtih customer");
         }
     }
